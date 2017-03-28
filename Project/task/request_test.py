9c148951d7d04b7a030254836fef2435b915b689
xuanzhenlei <zhenlei@starmerx.com>
2017-03-28 15:18:10 +0800

    request获取网页数据，提取有用信息，并以json格式保存——爬虫

---
+++ Project/task/request_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+# from urllib3 import request.Request
+# import http.cookiejar
+
+#url='https://book.douban.com/tag/%E5%8F%A4%E5%85%B8%E6%96%87%E5%AD%A6'
+# response=urllib.request.urlopen(url)
+# code=response.getcode()
+# html=response.read()
+# mystr=html.decode("utf8")
+# response.close()
+# print mystr
+
+
+# req=request.Request(url)
+# with request.urlopen(req) as f:
+#     print ('Status',f.status,f.reason)
+#     for k,v in f.getheaders():
+#         print ('%s:%s'%(k.v))
+#     print('Data:',f.read().decode('utf8'))
+
+
+
+# import urllib2
+# import re
+#
+# def main():
+#     url='https://book.douban.com/tag/%E5%8F%A4%E5%85%B8%E6%96%87%E5%AD%A6'
+#     req=urllib2.Request(url)
+#     resp=urllib2.urlopen(req)
+#     respHtml=resp.read()
+#     print "respHtml=",respHtml
+#     foundH1user=re.search('(?i)<meta\s?http-equiv="Content-Type"\s?content="text/html;\s?charset=(.+?)"\s?.?>', respHtml)
+#     print "foundH1user=",foundH1user
+#     if(foundH1user):
+#         h1user=foundH1user.group(1)
+#         print "h1user=",h1user
+# if __name__=="__main__":
+#     main()
+
+import urllib2
+import json
+import sys
+from bs4 import BeautifulSoup
+
+def http_get():
+    url='https://book.douban.com/tag/%E5%8F%A4%E5%85%B8%E6%96%87%E5%AD%A6'
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    response=urllib2.urlopen(url)
+    ret=response.read()
+    #print ret
+    soup=BeautifulSoup(ret,"lxml")
+    for i in soup.find_all('ul',class_="subject-list"):
+        all=""
+        for link in i.find_all('li', class_="subject-item"):
+            allBrands =all+link.get_text()
+            img = link.img.get('src')
+            #allBrands += ' %s' %img
+            all=allBrands+img
+        toJson=json.dumps(all,ensure_ascii=False,encoding='utf-8')
+        rttoJson=toJson.replace("\\n"," ")
+        return rttoJson
+if __name__ == "__main__":
+    print http_get()
+    fo=open('request.csv','w')
+    fo.write(http_get())
+    fo.close()
