60785fd9186abf06ea27e5fac26fb18abb059618
xuanzhenlei <zhenlei@starmerx.com>
2017-03-29 11:00:05 +0800

    测试其他函数

--- Project/AbstractFactory/test.py
+++ Project/AbstractFactory/test.py
@@ -27,3 +27,19 @@ print sch.get_name()
 print sch.get_address()
 print sch.get_history()
 sch.get_show()
+
+import commands
+import json
+def convertDict():
+    outStr = commands.getoutput("cat t1.txt | python -m json.tool")
+    toDict = eval(outStr)
+    #exec("toDict=" + outStr)
+    print type(toDict)
+    print toDict
+    return toDict
+if __name__ == "__main__":
+    fileToDict = convertDict()
+    keyList = ["1111","2222"]
+    for k in keyList:
+        del fileToDict["jobs"][k]
+    print json.dumps(fileToDict)
