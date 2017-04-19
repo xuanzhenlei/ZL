fe8ab4981b49ff134d065319b6e2b499fc8346ce
xuanzhenlei <zhenlei@starmerx.com>
2017-04-19 10:45:30 +0800

    scrapy测试，导出json格式数据

---
+++ Project/scrapy_test/scrapy_test/spiders/MininovaSpider.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy_test.items import TorrentItem
+
+
+class MininovaSpider(CrawlSpider):
+    name = 'mininova'
+    allowed_domains = ['mininova.org']
+    start_urls = ['http://www.mininova.org/today']
+    rules = [Rule(LinkExtractor(allow=['/tor/\d+']), 'parse_torrent')]
+
+    def parse(self, response):
+        torrent = TorrentItem()
+        # torrent['url'] = response.url
+        torrent['name'] = response.xpath("//h1/text()").extract()
+        # torrent['description'] = response.xpath("//div[@id='description']").extract()
+        # torrent['size'] = response.xpath("//div[@id='specifications']/p[2]/text()[2]").extract()
+        return torrent
