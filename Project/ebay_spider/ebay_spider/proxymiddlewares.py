836133e45f54eb13c7b70974a9ee13bb9c570a13
xuanzhenlei <zhenlei@starmerx.com>
2017-04-21 14:38:43 +0800

    ebay设置headers,ip,延时抓取避开网站反爬虫检查

---
+++ Project/ebay_spider/ebay_spider/proxymiddlewares.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import base64
+
+
+# Start your middleware class
+class ProxyMiddleware(object):
+    # overwrite process request
+    def process_request(self, request, spider):
+        # Set the location of the proxy
+        request.meta['proxy'] = "http://YOUR_PROXY_IP:PORT"
+
+        # Use the following lines if your proxy requires authentication
+        proxy_user_pass = "USERNAME:PASSWORD"
+        # setup basic authentication for the proxy
+        encoded_user_pass = base64.encodestring(proxy_user_pass)
+        request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
